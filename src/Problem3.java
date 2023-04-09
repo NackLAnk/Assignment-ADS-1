df4a7a915c7e6ea521879065923461bef707ef46
Alisher <www.at.7777@gmail.com>
2023-04-09 20:43:55 +0600

    Problem Task - 3

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,15 +1,13 @@
 public class Problem3 {
     public static String getPrimeComposite(int n){
-        if (n <= 1) {
+        if (n <= 1) { // Check if n is less than or equal to 1
             return "composite";
         }
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) { // Check for divisors up to the square root of n
             if (n % i == 0) {
                 return "composite";
             }
         }
-
-        return "prime";
+        return "prime"; // If no divisors were found, n is prime
     }
 }
